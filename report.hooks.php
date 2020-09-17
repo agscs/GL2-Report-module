034502b46d81c3d35b2b2a2b3e6bb0953976b0fb
agscs <ags_cs4@yahoo.com>
2020-09-17 19:07:04 +0100

    .

--- report.hooks.php
+++ report.hooks.php
@@ -1,9 +1,9 @@
 <?php
     new hook("profileLink", function ($profile) {
     	global $user;
-		if($user->id == $profile->id){
-			return false;
-		}
+  		if($user->id == $profile->id){
+  			return false;
+  		}
         return array(
             "url" => "?page=report&id=".$profile->id, 
             "text" => "Report"
